4e2fb19acff96582f8824067695d7a7dd70a902a
S-Mlangeni <smlangeni21@gmail.com>
2021-02-26 12:30:00 +0200

    update

--- routes/series/animation/route.js
+++ routes/series/animation/route.js
@@ -5,21 +5,21 @@ const path = require("path");
 router.get("/family-guy.jpg", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public", "/series", "/animation", "family-guy.jpg"));
 });
-router.get("/vid/family-guy.mp4", (req, res) => {
+router.get("/family-guy.mp4", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public", "/series", "/animation", "family-guy.mp4"));
 });
 
 router.get("/primal.jpg", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public", "/series", "/animation", "primal.jpg"));
 });
-router.get("/vid/primal.mp4", (req, res) => {
+router.get("/primal.mp4", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public", "/series", "/animation", "primal.mp4"));
 });
 
 router.get("/solar-opposites.jpg", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public", "/series", "/animation", "solar-opposites.jpg"));
 });
-router.get("/vid/solar-opposites.mp4", (req, res) => {
+router.get("/solar-opposites.mp4", (req, res) => {
     res.sendFile(path.join(__dirname, "../../../public", "/series", "/animation", "solar-opposites.mp4"));
 });
 
